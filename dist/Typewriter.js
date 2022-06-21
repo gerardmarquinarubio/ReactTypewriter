e6370341eb7623c67988153f8e51ff9d6ab6c1a0
Gerard Marquina Rubio <gerardmarquinarubio@gmail.com>
2022-06-21 18:28:24 +0200

    Working version

---
+++ dist/Typewriter.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Typewriter.module.css';
+const DEFAULT_MS = 30;
+export default function Typewriter({ text, speed = DEFAULT_MS, loop = false, random = DEFAULT_MS, delay = DEFAULT_MS, cursor = true }) {
+    const [currentStringIndex, setCurrentStringIndex] = useState(0);
+    const [currentTextIndex, setCurrentTextIndex] = useState(0);
+    useEffect(() => {
+        setTimeout(() => {
+            if (Array.isArray(text)) {
+                // if text is an array
+                if (currentTextIndex < text[currentStringIndex].length) {
+                    setCurrentTextIndex(currentTextIndex + 1);
+                }
+                else {
+                    if (currentStringIndex < text.length - 1) {
+                        setTimeout(() => {
+                            setCurrentTextIndex(0);
+                            setCurrentStringIndex(currentStringIndex + 1);
+                        }, delay);
+                    }
+                    else {
+                        if (loop) {
+                            setTimeout(() => {
+                                setCurrentTextIndex(0);
+                                setCurrentStringIndex(0);
+                            }, delay);
+                        }
+                    }
+                }
+            }
+            else {
+                // if text is string
+                if (currentTextIndex < text.length) {
+                    setCurrentTextIndex(currentTextIndex + 1);
+                }
+                else {
+                    if (loop) {
+                        setTimeout(() => {
+                            setCurrentTextIndex(0);
+                        }, delay);
+                    }
+                }
+            }
+        }, speed + (Math.random() * random));
+    });
+    if (Array.isArray(text))
+        return (React.createElement("span", null,
+            text[currentStringIndex].substring(0, currentTextIndex),
+            React.createElement("span", { className: styles.cursor }, cursor && '▎')));
+    else
+        return (React.createElement("span", null,
+            text.substring(0, currentTextIndex),
+            React.createElement("span", { className: styles.cursor }, cursor && '▎')));
+}
